cf88582a4da67d631e9a4db4daa5c1f865df876d
Nikodem <nikodem.lorenz@gmail.com>
2019-08-05 13:16:41 +0200

    Updates

--- sw.js
+++ sw.js
@@ -6,6 +6,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "1b8fce656a42557db8f3ccdce9de6180"
   },
   {
+    "url": "/moo-v/_nuxt/06b058fd6c732657da75.js",
+    "revision": "d185c74c7b37ef49c1737a3aaeb3c585"
+  },
+  {
     "url": "/moo-v/_nuxt/4b99716dc5d125f7c3b8.js",
     "revision": "c0189bf2064e6f6f619d0b8fc4cefd46"
   },
@@ -18,12 +22,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "8e876d610894be15f5622d239dc11616"
   },
   {
-    "url": "/moo-v/_nuxt/8f9848b6d3f0b177bbe3.js",
-    "revision": "076191868249ef1d521923843cde9d31"
-  },
-  {
-    "url": "/moo-v/_nuxt/d5829ec01b0ab115e762.js",
-    "revision": "d2148c28fc64276a19f87a961efe333a"
+    "url": "/moo-v/_nuxt/a342dd060c4eb1f6c39d.js",
+    "revision": "a529d0fe5984c656ae83b2d490d02523"
   }
 ], {
   "cacheId": "moo-v",
